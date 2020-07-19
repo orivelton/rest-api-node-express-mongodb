88fab229fed9967c6e1c72d4f8c2ad0ab805f72e
Orivelton Cesar <orivelton10@gmail.com>
2020-07-19 09:21:36 +0100

    chore: added verbs

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.patch('/', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+
+
 module.exports = router;
