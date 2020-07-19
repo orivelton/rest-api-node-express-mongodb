1c50cf98b83df111e8075dd4e66f0b5e8cd90ac4
Orivelton Cesar <orivelton10@gmail.com>
2020-07-19 09:17:04 +0100

    chore: Added router

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
 
+const subscribersRouter = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouter);
+
 app.listen(3000, () => console.log('Server Started'));
